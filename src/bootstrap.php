da556fac510958c8ec55db812754fa4ee6d3362a
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-06 14:43:58 +0200

    feat: add application bootstrap

---
+++ src/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(static function (string $class): void {
+    $prefix = 'QuantumBuilder\\';
+    if (!str_starts_with($class, $prefix)) {
+        return;
+    }
+    $relative = substr($class, strlen($prefix));
+    $path = __DIR__ . '/' . str_replace('\\', '/', $relative) . '.php';
+    if (is_file($path)) {
+        require $path;
+    }
+});
+
+use QuantumBuilder\Auth;
+use QuantumBuilder\Database;
+
+$db = new Database();
+$db->migrate();
+$auth = new Auth($db->pdo());
+$auth->bootstrapSession();
