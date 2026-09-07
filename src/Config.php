e259bc7d18e41b0358bd4feecc079fd2594511c9
Starlight Unit Studios <pschildgen87@gmail.com>
2026-09-07 20:48:16 +0200

    chore: update alpha2 fallback version

--- src/Config.php
+++ src/Config.php
@@ -18,7 +18,7 @@ final class Config
 
     public static function version(): string
     {
-        return (string) (getenv('QB_VERSION') ?: '0.1.0-alpha1');
+        return (string) (getenv('QB_VERSION') ?: '0.1.0-alpha2');
     }
 
     public static function sessionSecret(): string
